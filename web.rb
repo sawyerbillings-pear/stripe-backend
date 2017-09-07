765dc862034cb08756ff530c057b0e878ddbc609
Sawyer Billings <sbilling@bowdoin.edu>
2017-09-06 20:36:57 -0400

    Update web.rb

--- web.rb
+++ web.rb
@@ -20,7 +20,7 @@ post '/ephemeral_keys' do
   begin
     key = Stripe::EphemeralKey.create(
       {customer: @customer.id},
-      {stripe_version: params["api_version"]}
+      {stripe_version: "2017-08-15"}
     )
   rescue Stripe::StripeError => e
     status 402
