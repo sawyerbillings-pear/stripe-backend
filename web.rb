daeb7041d362b50bb034e9326086cf34f9612903
Sawyer Billings <sbilling@bowdoin.edu>
2017-10-16 00:36:31 -0400

    Update web.rb

--- web.rb
+++ web.rb
@@ -45,7 +45,7 @@ post '/charge' do
                                        :currency => "usd",
                                        :customer => @customer.id,
                                        destination: {
-                                       amount: params[:amount].to_i - (params[:amount].to_i * 0.079 - 30),
+                                       amount: params[:amount].to_f - (params[:amount].to_f * 0.079 - 30),
                                        account: params[:destination],
                                        },
                                        :source => source,
